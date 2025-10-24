857e04c7766b65ac9db334831d8b91bc5fe78de2
Ayabulela Mahlathini <ayabulelamahlathini@gmail.com>
2025-10-24 14:20:22 +0200

    fix: amplify initialization at top level

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Amplify } from "aws-amplify";
 import AppShell, { type AppShellProps } from "./app/AppShell";
-import './config/amplify';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID || "",
+      userPoolClientId: import.meta.env.VITE_AWS_USER_POOL_CLIENT_ID || "",
+    },
+  },
+});
 
 function App(props: Readonly<AppShellProps>) {
   return <AppShell {...props} />;
